a09100487c9800c235aefe42206245b7b525a20a
Chris Williams <spider.mane.web@gmail.com>
2023-01-29 02:01:14 -0500

    added redis

--- app/public/index.php
+++ app/public/index.php
@@ -4,11 +4,17 @@ use App\Repository\TranslationRepository;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+$client = \Symfony\Component\Cache\Adapter\RedisAdapter::createConnection(
+    "redis://{$_ENV['REDIS_HOST']}:{$_ENV['REDIS_PORT']}"
+);
+
+$cacheAdapter = new \Symfony\Component\Cache\Adapter\RedisAdapter($client);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $translationRepository = new TranslationRepository();
+    $translationCache = new \App\Cache\TranslationCache($cacheAdapter, new TranslationRepository());
+    $translation = $translationCache->findForLanguage($_POST['language'], $_POST['phrase']) ?: 'Translation not found...';
 
-    $translation = $translationRepository->findForLanguage($_POST['language'], $_POST['phrase']) ?: 'Translation not found...';
 } else {
 
     $languageRepository = new \App\Repository\LanguageRepository();
@@ -19,12 +25,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Translate This</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
     <style>
         .bd-placeholder-img {
@@ -86,45 +92,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </style>
 
 </head>
-
 <body>
-    <div class="bg-dark text-secondary px-4 py-5 text-center" style="height: 100vh">
-        <div class="py-5">
-            <h1 class="display-5 fw-bold text-white">Translate This</h1>
-            <div class="col-lg-6 mx-auto">
-                <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
-                    <p class="fs-5 mb-4"><?php echo $translation; ?></p>
-                    <a href="/">Translate another</a>
-                <?php else : ?>
+<div class="bg-dark text-secondary px-4 py-5 text-center" style="height: 100vh">
+    <div class="py-5">
+        <h1 class="display-5 fw-bold text-white">Translate This</h1>
+        <div class="col-lg-6 mx-auto">
+            <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+                <p class="fs-5 mb-4"><?php echo $translation; ?></p>
+                <a href="/">Translate another</a>
+            <?php else: ?>
 
-                    <p class="fs-5 mb-4">Select a language and enter your word(s).</p>
+                <p class="fs-5 mb-4">Select a language and enter your word(s).</p>
 
-                    <form method="post">
-                        <div class="row g-3">
-                            <div class="col">
-                                <select name="language" class="form-select" aria-label="Default select example">
-                                    <option selected>Select a language</option>
-                                    <?php foreach ($languages as $language) : ?>
-                                        <option value="<?php echo $language->getId(); ?>">
-                                            <?php echo $language->getName(); ?>
-                                        </option>
-                                    <?php endforeach; ?>
-                                </select>
-                            </div>
-                            <div class="col">
-                                <input name="phrase" type="text" class="form-control" placeholder="Phrase" aria-label="Phrase">
-                            </div>
-                            <div class="d-grid gap-2">
-                                <button class="btn btn-outline-info" type="submit">Translate</button>
-                            </div>
+                <form method="post">
+                    <div class="row g-3">
+                        <div class="col">
+                            <select name="language" class="form-select" aria-label="Default select example">
+                                <option selected>Select a language</option>
+                                <?php foreach ($languages as $language): ?>
+                                    <option value="<?php echo $language->getId(); ?>">
+                                        <?php echo $language->getName(); ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
-                    </form>
+                        <div class="col">
+                            <input name="phrase" type="text" class="form-control" placeholder="Phrase" aria-label="Phrase">
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-outline-info" type="submit">Translate</button>
+                        </div>
+                    </div>
+                </form>
 
-                <?php endif; ?>
+            <?php endif; ?>
 
-            </div>
         </div>
     </div>
+</div>
 </body>
-
 </html>
+
